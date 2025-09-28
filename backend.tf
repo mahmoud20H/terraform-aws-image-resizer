e59460de19e8b858f540f673c79f764c4d4a13f7
MahmoudHussein20 <mahmoud.hussein.ebrahim@vultara.com>
2025-09-28 17:29:49 +0300

    Configure S3 backend for Terraform state

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    # Configuration will be provided via CLI arguments
+    # in the GitHub Actions workflows
+  }
+}
