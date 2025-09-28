149f3aa905d53d56f4ea31adc36c5cd2a3586dba
MahmoudHussein20 <mahmoud.hussein.ebrahim@vultara.com>
2025-09-28 17:38:03 +0300

    Fix duplicate required providers configuration

--- backend.tf
+++ backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     # Configuration will be provided via CLI arguments
     # in the GitHub Actions workflows
